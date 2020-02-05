49d21e1880d8d364475704f9f9abd36c93995236
daishaowen <872961086@qq.com>
2020-02-05 15:47:06 +0800

    unittest

---
+++ mounttest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/enb python
+import unittest
+from mounttab2 import parse_mounts
+
+class TestMount(unittest.TestCase):
+    """
+    """
+
+    def test_parsemount(self):
+        """
+        """
+        result = parse_mounts()
+        self.assertIsInstance(result,list)
+        self.assertIsInstance(result[0],tuple)
+
+    def test_rootest4(self):
+        """
+        """
+        result = parse_mounts()
+        for line in result:
+            if line[1] == '/' and line[2] != 'rootfs':
+                self.assertEqual(line[2], 'ext4')
+
+
+if __name__ == '__main__':
+    unittest.main()
